cbd798b63ae6c67de154bcbd5158d5b50b05f719
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-05-07 18:23:05 +0300

    Updates

--- WebServiceSide/WebServiceSide/WebService1.asmx.cs
+++ WebServiceSide/WebServiceSide/WebService1.asmx.cs
@@ -1,26 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Data.SqlClient;
 using System.Web.Services;
 
 namespace WebServiceSide
 {
-    /// <summary>
-    /// Summary description for WebService1
-    /// </summary>
     [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
-    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-    // [System.Web.Script.Services.ScriptService]
+
     public class WebService1 : System.Web.Services.WebService
     {
 
         [WebMethod]
-        public string HelloWorld()
+        public SqlConnection GetDatabaseConnection() 
         {
-            return "Hello World";
+            SqlConnection connection = new SqlConnection
+            {
+                ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=D:\\FACULTATE\\AN3SEM2\\II\\LAB7-LAB8\\PROJECT\\DOTNET-TEST-PROJECT-WEB-SERVICES\\WEBSERVICESIDE\\WEBSERVICESIDE\\APP_DATA\\DATABASE1.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"
+            };
+
+            return connection;
         }
     }
 }
